41048b3203ceda4ef23b3ea40cc27e0f0d5e36ce
Juan Leon <jleon@alertlogic.com>
2018-06-05 13:55:35 -0500

    Adding another example (cards)

--- src/app/modules/test/test.module.ts
+++ src/app/modules/test/test.module.ts
@@ -3,18 +3,22 @@ import { CommonModule } from '@angular/common';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatCardModule} from '@angular/material/card';
 import { UiTestComponent } from './ui-test/ui-test.component';
+import { UiTestCardComponent } from './ui-test-card/ui-test-card.component';
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
-    MatButtonModule, MatCheckboxModule
+    MatButtonModule, MatCheckboxModule,
+    MatCardModule
   ],
-  declarations: [UiTestComponent],
+  declarations: [UiTestComponent, UiTestCardComponent],
   exports: [
-    UiTestComponent
+    UiTestComponent,
+    UiTestCardComponent
   ]
 })
 export class TestModule { }
